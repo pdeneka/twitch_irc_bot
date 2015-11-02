7c9c93d762f1f8cfe6d5220af1c0295b5dc869cb
pdeneka <pdeneka@gmail.com>
2015-11-02 00:34:57 -0800

    Tweak
    Updated user_commands.py to reflect new credentials setup

--- user_commands.py
+++ user_commands.py
@@ -1,19 +1,24 @@
-import mssql_credentials as m
+import credentials
 import pyodbc
+import time
+
+credentials = 'DRIVER={SQL Server};SERVER=' + credentials.SQL_SERVER + \
+              ';DATABASE=' + credentials.SQL_DATABASE + \
+              ';UID=' + credentials.SQL_USER_ID + \
+              ';PWD=' + credentials.SQL_PASSWORD
 
 def add_user(user, 0)
- credentials = 'DRIVER={SQL Server};SERVER=' + m.server + ';DATABASE=' + m.database + ';UID=' + m.user_id + ';PWD=' + m.password
  connection = pyodbc.connect(credentials)
  cursor = connection.cursor()
- #if user does not exist in table
- #cursor.execute("INSERT INTO Twitch_Users (twitch_user, real_name, Skype, Security_ID, Last_seen)
- # VALUES(?, ?, ?, ?, ?)", level, '', '', user, python.getdatetime())
- cursor.commit()
+ cursor.execute("SELECT COUNT(twitch_account_id) as CNT FROM Twitch_Users WHERE = ?")
+ row = cursor.fetchone()
+ if(row.CNT <= 0):
+  cursor.execute("INSERT INTO Twitch_Users (twitch_user_account, real_name, Skype, Security_ID, Last_seen) VALUES(?, ?, ?, ?, ?)", level, '', '', user, strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+  cursor.commit()
  connection.close
  
 def set_level(user, level):
  add_user(user) #add user if not exist
- credentials = 'DRIVER={SQL Server};SERVER=' + m.server + ';DATABASE=' + m.database + ';UID=' + m.user_id + ';PWD=' + m.password
  connection = pyodbc.connect(credentials)
  cursor = connection.cursor()
  cursor.execute("UPDATE Twitch_Users SET Security_ID = ? WHERE twitch_user_account = ?", level, user)
